3175b2761aa196d3c9f005f1d0e8352395c1b11f
Paulo Silva <pauloalmeida1288@gmail.com>
2025-09-18 13:38:40 +0100

    added menu interaction, not finished

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -4,18 +4,70 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private Button nextSceneButton; // assign in Inspector
+    [Header("Menu")]
+    [SerializeField] private GameObject menu;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button leaveGameButton;
 
+    [Space(10)]
+    [Header("Options")]
+    [SerializeField] private GameObject options;
+    [SerializeField] private Slider soundSlider;
+    [SerializeField] private Slider brightnessSlider;
+    [SerializeField] private Button exitOptions;
+
+    [Space(10)]
+    [Header("Credits")]
+    [SerializeField] private GameObject credits;
+    [SerializeField] private Button creditsExit;
     private void Awake()
     {
-        // Add listener to button
-        nextSceneButton.onClick.AddListener(LoadNextScene);
+        // Menu
+        startButton.onClick.AddListener(LoadNextScene);
+        creditsButton.onClick.AddListener(TurnCreditsOn);
+        optionsButton.onClick.AddListener(TurnOptionsOn);
+
+
+        //Options
+
+        //Credits
     }
 
     private void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private void TurnOptionsOn()
+    {
+        options.SetActive(true);
+        TurnMenuOff();
+    }
+    private void TurnOptionsOff()
+    {
+        options.SetActive(false);
+        TurnMenuOn();
+    }
+    private void TurnCreditsOn()
+    {
+        credits.SetActive(true);
+        TurnMenuOff();
+    }
+    private void TurnCreditsOff()
+    {
+        credits.SetActive(false);
+        TurnMenuOn();
+    }
+    private void TurnMenuOn()
+    {
+        menu.SetActive(true);
+    }
+    private void TurnMenuOff()
+    {
+        menu.SetActive(false);
     }
 
 }
